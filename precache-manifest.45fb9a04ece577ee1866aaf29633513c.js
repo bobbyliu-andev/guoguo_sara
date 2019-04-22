6b354e4d0c8c2bb9c46d7902a80518456dc2d25e
bobbyliu.andev <bobbyliu.andev@gmail.com>
2019-04-21 22:25:03 -0700

    Updates

---
+++ precache-manifest.45fb9a04ece577ee1866aaf29633513c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd2efa665e51700740e2ed7861fcb3c",
+    "url": "/guoguo_sara/static/media/sara_qr.cfd2efa6.jpeg"
+  },
+  {
+    "revision": "4ea2a2c4bdb25e215d2c3ab5dfe7a88d",
+    "url": "/guoguo_sara/static/media/logo2.4ea2a2c4.jpeg"
+  },
+  {
+    "revision": "3dd261696db8fb4edeb50f76a8eff3be",
+    "url": "/guoguo_sara/static/media/logo1.3dd26169.jpeg"
+  },
+  {
+    "revision": "38c1157d193f83b3f45d9913a6403d7a",
+    "url": "/guoguo_sara/static/media/ios_qr.38c1157d.png"
+  },
+  {
+    "revision": "0cf688b9c596f0d42703",
+    "url": "/guoguo_sara/static/js/runtime~main.116437fb.js"
+  },
+  {
+    "revision": "59326469584a174c0925",
+    "url": "/guoguo_sara/static/js/main.557a5907.chunk.js"
+  },
+  {
+    "revision": "a05b0dabe49b395153a0",
+    "url": "/guoguo_sara/static/js/2.cbe0269c.chunk.js"
+  },
+  {
+    "revision": "59326469584a174c0925",
+    "url": "/guoguo_sara/static/css/main.cf2cdc7a.chunk.css"
+  },
+  {
+    "revision": "f846d89a8665d051388f76b3e65bbb0b",
+    "url": "/guoguo_sara/index.html"
+  }
+];
